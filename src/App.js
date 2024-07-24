14500a48d54ad4ea3c712ee227bfceb40a7f0535
Deepak Ramani <deepakramani65@gmail.com>
2024-07-24 21:02:37 +0530

    update routes settings

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminLogin from "./screens/admin/AdminLogin";
-import AdminDashboard from "./screens/admin/AdminDashboard"
+import AdminDashboard from "./screens/admin/AdminDashboard";
 import Home from "./screens/userinterface/Home";
 import ProductDetailsUI from './screens/userinterface/ProductDetailsUI';
-import ProductCart from "./screens/userinterface/ProductCart"
+import ProductCart from "./screens/userinterface/ProductCart";
 import LogInScreen from './screens/userinterface/LoginScreen';
 import FilterPage from './screens/userinterface/FilterPage';
-
+import NotFound from './screens/userinterface/NotFound'; // Add a 404 component if necessary
 
 function App() {
  return (
-   <div>
-    <Router>
+   <Router>
      <Routes>
-        
-       <Route  element={<AdminLogin/>} path={'/adminlogin'}/>
-       <Route  element={<AdminDashboard/>} path={'/admindashboard/*'}/>
-       <Route  element={<Home />} path={'/home'}/>
-       <Route  element={<ProductDetailsUI />} path={'/productdetailsui'}/>
-       <Route  element={<ProductCart />} path={'/productcart'}/>
-       <Route element={<LogInScreen/>} path={'/loginscreen'} />
-       <Route element={<FilterPage/>} path={'/filterpage'} />
-
-
-
-
+       <Route path="/" element={<Home />} />
+       <Route path="/adminlogin" element={<AdminLogin />} />
+       <Route path="/admindashboard/*" element={<AdminDashboard />} />
+       <Route path="/productdetailsui" element={<ProductDetailsUI />} />
+       <Route path="/productcart" element={<ProductCart />} />
+       <Route path="/loginscreen" element={<LogInScreen />} />
+       <Route path="/filterpage" element={<FilterPage />} />
+       <Route path="*" element={<NotFound />} /> {/* Wildcard route for 404 pages */}
      </Routes>
-    </Router>
-   </div>
+   </Router>
  );
 }
 
---
+++ src/screens/userinterface/NotFound.js
@@ -0,0 +1,6 @@
+export default function NotFound(){
+    return(
+    <div>
+        <h2>Page Not Found</h2>
+    </div>)
+}
