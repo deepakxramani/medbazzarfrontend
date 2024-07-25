0fbcde66477e0f236d3637426d1f726e596741a0
Deepak Ramani <deepakramani65@gmail.com>
2024-07-25 13:42:41 +0530

    fix pi page button

---
+++ src/components/userinterface/PlusMinusComp3.jsx
@@ -0,0 +1,46 @@
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { useEffect, useState } from "react";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import  {useTheme}  from '@mui/material/styles';
+
+
+export default function PlusMinusComp3(props){
+    
+    const [value,setValue]=useState(props.qty)
+    useEffect(function(){
+        setValue(props.qty)
+    },[props.qty,value])
+
+    const theme = useTheme()
+      const matchesMd = useMediaQuery(theme.breakpoints.down('md'));
+      const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+      const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
+
+    const handleMinus=()=>{
+        if(value>=1)
+        {setValue((prev)=>prev-1)
+        var v=value
+        v=v-1
+        props.onChange(v)}
+    }
+    const handlePlus=()=>{
+        setValue((prev)=>prev+1)
+        var v=value
+        v=v+1
+        props.onChange(v)
+    }
+    return(<div style={{display:'flex'}}>
+        
+        {value==0?<div className="product-actions" style={{padding:0}}>
+            <button style={{width: matchesSM?'100px':props.width, height: matchesSM?'40px':props.height, fontSize:matchesSM?'15px':props.fontSize,display:'flex',alignItems:'center',justifyContent:'center'}} onClick={handlePlus}>Add <ShoppingCartOutlinedIcon  style={{marginLeft:10, fontSize: props.fontSize}}/></button>
+        </div>:
+        <div style={{width: matchesSM?'100px':props.width, height: matchesSM?'40px':props.height, background:'#00391c',display:'flex',alignItems:'center',justifyContent:'space-around',gap:matchesSM?'5px':'10px',backgroundColor:'#007bff',border: 'none',padding: '3px 10px',borderRadius: 8,fontWeight:600}} >
+            <div onClick={handleMinus} style={{color:'#fff',fontSize: matchesMd?'13px':'14px', fontWeight:600,cursor:'pointer',marginLeft:5}}><RemoveIcon style={{fontSize:matchesMd?'13px':'14px'
+            ,fontWeight:800}}/></div>
+            <div style={{color:'#fff',fontSize:matchesMd?'13px':'14px',fontWeight:700}}>{value}</div>
+            <div onClick={handlePlus} style={{color:'#fff',fontSize:matchesMd?'13px':'14px',fontWeight:600,cursor:'pointer',marginRight:5}}><AddIcon style={{fontSize:matchesMd?'13px':'14px', fontWeight:800}}/></div>
+        </div>}
+        </div>)
+}
--- src/components/userinterface/ProductInformation.jsx
+++ src/components/userinterface/ProductInformation.jsx
@@ -13,7 +13,7 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import parse from 'html-react-parser';
 import {useDispatch,useSelector} from 'react-redux';
-import PlusMinusComp from "./PlusMinusComp";
+import PlusMinusComp3 from "./PlusMinusComp3";
 
 
 
@@ -142,7 +142,7 @@ export default function ProductInformation(props){
                 </Grid>
                 <Grid item xs={12}>
                     <div style={{display:'flex', gap:40,padding:'15px 0px'}}>
-                    <PlusMinusComp  qty={product?.qty} onChange={(v)=>handleChange(v,product)} width={200} height={50} fontSize={18} margin={'0px 10px'} />
+                    <PlusMinusComp3  qty={product?.qty} onChange={(v)=>handleChange(v,product)} width={200} height={50} fontSize={18} margin={'0px 10px'} />
                     <button onClick={()=>navigate('/')} style={{display:'inline-flex', alignItems:'center', justifyContent:'center',width: matchesSM?'100px':'200px', height: matchesSM?'40px':'50px', padding:'10px 15px', gap:10, fontSize: matchesSM?12:15, fontWeight:600,color:'#fff',background:'#228B22',border:'none',borderRadius:10, cursor:'pointer'}}>CONTINUE {matchesSM?'':'SHOPPING'}<LocalMallIcon style={{color:'#fff',fontSize:matchesSM?15:18}}/></button>
                     </div>
                 </Grid>
