a6e7b5375d1fa5f9b3e04779c31c54d3c199ba6d
Deepak Ramani <deepakramani65@gmail.com>
2024-09-04 21:30:28 +0530

    bug fixes

--- src/services/FetchNodeServices.js
+++ src/services/FetchNodeServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-var serverURL="http://localhost:5000"       //   http://localhost:5000 or https://medbazzarbackend.onrender.com
+var serverURL = process.env.SERVER_URL;
 const postData=async(url,body)=>{
     try
     {
